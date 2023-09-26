05ea76700176a4afa9e51b3fe9a59790330c3577
BEKIRKSU <bekirkscu@gmail.com>
2023-09-26 15:10:12 +0100

    Install libraries easily

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from turtle import Turtle, Screen
 
 
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
+ttt = Turtle()
+ttt.shape("turtle")
 # Use documentation to find different shapes, particular functions etc such as .shape("")
 # Or Google it
-timmy_the_turtle.color("DarkSeaGreen")
-timmy_the_turtle.right(77)
-timmy_the_turtle.forward(77)
-timmy_the_turtle.right(77)
+ttt.color("DarkSeaGreen")
+ttt.right(77)
+ttt.forward(77)
+ttt.right(77)
 
 # timmy_the_turtle.right(90)
 # timmy_the_turtle.forward(100)
@@ -22,8 +22,17 @@ timmy_the_turtle.right(77)
 # instead use the for loop and range operator:
 
 for _ in range(4):
-    timmy_the_turtle.right(90)
-    timmy_the_turtle.forward(100)
+    ttt.right(90)
+    ttt.forward(100)
+    # to change the name of anything we named we can right-click, refactor, rename and change it.
+#     timmy_the_turtle turned into ttt.
+
+import heroes
+# you can just write import from a package that isn't downloaded, and pyCharm will ask if you
+# want to download it, very clear and straightforward.
+print(heroes.gen())
+
+
 
 
 
