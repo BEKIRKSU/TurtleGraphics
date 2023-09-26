b8047723a90bfb7ff47ee9b6314b0bb2bb145548
BEKIRKSU <bekirkscu@gmail.com>
2023-09-26 14:30:58 +0100

    -

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 
+
+
+
+
+
+
+
+
+
 screen = Screen()
 screen.exitonclick()
+# this will exit on click, so when you click the window it will exit. So we move it to the bottom of the
+# file
