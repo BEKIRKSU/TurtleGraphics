7b461339979beecf4edf211be258e9a6f6bcdf86
BEKIRKSU <bekirkscu@gmail.com>
2023-09-27 15:01:04 +0100

    New function for random colour

--- main.py
+++ main.py
@@ -40,6 +40,14 @@ Turtle.colormode(255)
 
 # colours = ["blue", "green", "orange", "yellow", "black", "red", "DeepSkyBlue", "cyan4", "coral", "AntiqueWhite3"]
 
+# new function to return random colour using rgb"
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+
+
 def draw_shape(num_sides):
     angle = 360 / num_sides
     for _ in range(num_sides):
